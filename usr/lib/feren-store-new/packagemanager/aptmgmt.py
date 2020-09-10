68ac5d6e2c9c9b2aedf9ce5f522bdc0969a9f602
Dominic Hayes <ferenosdev@outlook.com>
2020-09-10 18:36:57 +0100

    aptmgmt works now, yay

--- usr/lib/feren-store-new/packagemanager/aptmgmt.py
+++ usr/lib/feren-store-new/packagemanager/aptmgmt.py
@@ -2,98 +2,92 @@
 
 ###################################################################################
 # APT Management Python Script - use me for percentage obtaining in-GUI           #
+#                                                                                 #
+#  Note: This was stupidly painful to get working, and took A LOT of attempts     #
+#  so for your sanity please just use this as it is and don't bother with editing #
+#  it, etc, unless you REALLY know what you're doing. Some credit goes to         #
+#  https://github.com/schlomo/apt-install/blob/master/apt-install.py, by the way. #
 ###################################################################################
 
 import sys
 import os
 
-import aptdaemon.client
+from aptdaemon.client import AptClient
 import aptdaemon.errors
 import aptdaemon.enums
-import apt
+import gi
+from gi.repository import GLib
 
-inprogress = False
-apt_client = aptdaemon.client.AptClient()
+class APTMgmt():
+    def __init__(self):
+        self.apt_client = AptClient()
+        self.apt_transaction = None
+        self.packages = []
+        self.loop = None
 
+    def confirm_changes(self, apt_transaction):
+        self.apt_transaction = apt_transaction
+        try:
+            self.run_transaction()
+        except Exception as e:
+            print(e)
 
-def progress_apt_cb(apt_transaction, progress):
-    print(progress / 100)
+    def on_error(self, error):
+        print("ERROR", str([aptdaemon.enums.get_error_string_from_enum(error.code), aptdaemon.enums.get_error_description_from_enum(error.code)]))
+
+    def run_transaction(self):
+        self.apt_transaction.connect("finished", self.on_transaction_finished)
+        self.apt_transaction.connect("progress-changed", self.on_transaction_progress)
+        self.apt_transaction.connect("error", self.on_transaction_error)
+        self.apt_transaction.run(reply_handler=lambda: None, error_handler=self.on_error)
     
+    def on_transaction_progress(self, apt_transaction, progress):
+        if not apt_transaction.error:
+            print(progress)
 
-def apt_run_transaction(apt_transaction):
-    print(2)
-    inprogress = True
-    apt_transaction.connect("finished", on_transaction_finished)
-    apt_transaction.connect("progress-changed", progress_apt_cb)
-    apt_transaction.connect("error", on_transaction_error)
-    apt_transaction.run(reply_handler=lambda: None, error_handler=on_error)
+    def on_transaction_error(self, apt_transaction, error_code, error_details):
+        print("ERROR", error_code, error_details)
 
-
-def apt_confirm_changes(apt_transaction):
-    print(1)
-    
-    try:
-        apt_run_transaction(apt_transaction)
-    except Exception as e:
-        print("1e", e)
-        inprogress = False
-
-
-def on_error(error):
-    print(str("ERROR", [aptdaemon.enums.get_error_string_from_enum(error.code), aptdaemon.enums.get_error_description_from_enum(error.code)]))
-
-
-def on_transaction_error(apt_transaction, error_code, error_details):
-    on_error(apt_transaction.error)
-
-
-def on_transaction_finished(apt_transaction, exit_state):
-    # finished signal is always called whether successful or not
-    # Only call here if we succeeded, to prevent multiple calls
-    if (exit_state == aptdaemon.enums.EXIT_SUCCESS) or apt_transaction.error_code == "error-not-authorized":
+    def on_transaction_finished(self, apt_transaction, exit_state):
         print("DONE")
-        inprogress = False
+        self.loop.quit()
+        
 
+loop = GLib.MainLoop()
+aptmgmt = APTMgmt()
 
-def apt_install(packagename):
-    print(0, packagename)
-    apt_client.install_packages([packagename],
-                                reply_handler=apt_confirm_changes,
-                                error_handler=on_error) # dbus.DBusException
-    while inprogress == False:
-        pass
-    while inprogress == True:
-        pass
+def apt_install():
+    aptmgmt.apt_client.install_packages(aptmgmt.packages,
+                                reply_handler=aptmgmt.confirm_changes,
+                                error_handler=aptmgmt.on_error) # dbus.DBusException
     
 
-def apt_upgrade(packagename):
-    apt_client.upgrade_packages([packagename],
-                                reply_handler=apt_confirm_changes,
-                                error_handler=on_error) # dbus.DBusException
-    while inprogress == False:
-        pass
-    while inprogress == True:
-        pass
+def apt_upgrade():
+    aptmgmt.apt_client.upgrade_packages(aptmgmt.packages,
+                                reply_handler=aptmgmt.confirm_changes,
+                                error_handler=aptmgmt.on_error) # dbus.DBusException
     
 
-def apt_remove(packagename):
-    apt_client.remove_packages([packagename],
-                                reply_handler=apt_confirm_changes,
-                                error_handler=on_error) # dbus.DBusException
-    while inprogress == False:
-        pass
-    while inprogress == True:
-        pass
+def apt_remove():
+    aptmgmt.apt_client.remove_packages(aptmgmt.packages,
+                                reply_handler=aptmgmt.confirm_changes,
+                                error_handler=aptmgmt.on_error) # dbus.DBusException
 
 
 #Rule of Python: [0] counts as an item in the length count.
 if len(sys.argv) != 3:
     print("Incorrect number of arguments.")
     exit(1)
+    
+aptmgmt.packages = sys.argv[2:]
+aptmgmt.loop = loop
 
 if sys.argv[1] == "install":
-    apt_install(sys.argv[2])
+    GLib.timeout_add(10000,apt_install)
 elif sys.argv[1] == "upgrade":
-    apt_upgrade(sys.argv[2])
+    GLib.timeout_add(10000,apt_upgrade)
 elif sys.argv[1] == "remove":
-    apt_remove(sys.argv[2])
+    GLib.timeout_add(10000,apt_remove)
+    
+    
+loop.run()
